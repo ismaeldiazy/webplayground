d9fc87992661657ad21eb84c40d2f58bb66ff176
ismaeldiazy <ismaeldiazy@gmail.com>
2020-06-02 23:32:36 +0200

    Created auth paths and added login template

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
+    # Auth paths
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
