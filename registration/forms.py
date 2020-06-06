36682d6735367102db61779fbb071295cb7f4149
ismaeldiazy <ismaeldiazy@gmail.com>
2020-06-06 17:26:59 +0200

    Improved profile form

--- registration/forms.py
+++ registration/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class UserCreationFormWithEmail(UserCreationForm):
     email = forms.EmailField(required=True, help_text="Requerido, 254 caracteres como máximo y debe ser válido")
@@ -19,3 +20,13 @@ class UserCreationFormWithEmail(UserCreationForm):
             # IF exists, raise error
             raise forms.ValidationError("El email ya está registrado. Prueba con otro.")
         return email
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio', 'link']
+        widgets = {
+            'avatar': forms.ClearableFileInput(attrs={'class':'form-control-file mt3'}),
+            'bio': forms.Textarea(attrs={'class':'form-control', 'rows':3, 'placeholder':'Biografía'}),
+            'link': forms.URLInput(attrs={'class':'form-control', 'placeholder':'Enlace'}),
+        }
