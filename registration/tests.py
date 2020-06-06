caeea9041bd629cd5fc37132b0edbc5dc081baad
ismaeldiazy <ismaeldiazy@gmail.com>
2020-06-06 21:16:24 +0200

    Created unit test for testing if the profile is created automatically after a user is registered

--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user('testuser', 'test@testing.com', 'test1234')
+
+    def test_profile_exists(self):
+        exists = Profile.objects.filter(user__username='testuser').exists()
+        self.assertEqual(exists, True)
