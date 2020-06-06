2c154dc2662b48f7e28b23f8bd735ab43d73b3d2
ismaeldiazy <ismaeldiazy@gmail.com>
2020-06-06 19:31:54 +0200

    Now the profile email address can be modified

--- registration/forms.py
+++ registration/forms.py
@@ -30,3 +30,17 @@ class ProfileForm(forms.ModelForm):
             'bio': forms.Textarea(attrs={'class':'form-control', 'rows':3, 'placeholder':'Biografía'}),
             'link': forms.URLInput(attrs={'class':'form-control', 'placeholder':'Enlace'}),
         }
+
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required=True, help_text="Requerido, 254 caracteres como máximo y debe ser válido")
+
+    class Meta:
+        model = User
+        fields = ['email']
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if 'email' in self.changed_data:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("El email ya está registrado, prueba con otro.")
+        return email
