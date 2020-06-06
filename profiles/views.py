bf79f1941950dd0a65334082bcfa77225a25bd48
ismaeldiazy <ismaeldiazy@gmail.com>
2020-06-06 23:08:48 +0200

    Created profiles app to create list view and detail view of the profiles.

---
+++ profiles/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from registration.models import Profile
+from django.shortcuts import get_object_or_404
+
+# Create your views here.
+class ProfileListView(ListView):
+    model = Profile
+    template_name = 'profiles/profile_list.html'
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = 'profiles/profile_detail.html'
+
+    def get_object(self):
+        return get_object_or_404(Profile, user__username=self.kwargs['username'])
