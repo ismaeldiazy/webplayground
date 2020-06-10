618305d3429c0fd0fcf44c075deddc5dd415657c
ismaeldiazy <ismaeldiazy@gmail.com>
2020-06-11 00:13:33 +0200

    Messages send by async requests using Fetch API.+

--- messenger/views.py
+++ messenger/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import Thread
+from .models import Thread, Message
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
 from django.http import Http404, JsonResponse
-
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 @method_decorator(login_required, name="dispatch")
@@ -24,7 +24,15 @@ class ThreadDetail(DetailView):
         return obj
 
 def add_message(request, pk):
-    print(request.GET)
     json_response = {'created':False}
+    if request.user.is_authenticated:
+        content = request.GET.get('content', None)
+        if content:
+            thread = get_object_or_404(Thread, pk=pk)
+            message = Message.objects.create(user=request.user, content=content)
+            thread.messages.add(message)
+            json_response['created'] = True
+    else:
+        raise Http404("User is not authenticated.")
     # Convert python dic to Json obj
     return JsonResponse(json_response)
